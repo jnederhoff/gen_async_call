7bc76c38262d39aefc0a5f33ba4a2df31e3f96ec
Jeremy Nederhoff <jeremy.nederhoff@gmail.com>
2019-07-22 17:17:41 -0500

    Add support for GenStateMachine

---
+++ lib/gen_asyn_call/gen_state_machine.ex
@@ -0,0 +1,132 @@
+defmodule GenAsyncCall.GenStateMachine do
+  @callback handle_async_reply({:ok, reply :: any} | {:error, reason :: term}, tag :: term, state :: term, data :: term) :: :gen_statem.event_handler_result(GenStateMachine.state)
+
+  @callback interposer(reply :: any, GenAsyncCall.call_refs, state :: term, data :: term) :: any
+
+  @callback push_refs(state :: term, data :: term, GenAsyncCall.call_refs) :: {new_state :: term, new_data ::term}
+
+  @callback pop_refs(state :: term, data :: term, reference) :: {GenAsyncCall.call_refs | nil, new_state :: term, new_data :: term}
+
+  @optional_callbacks handle_async_reply: 4
+
+  defmacro __using__(opts) do
+    quote location: :keep, bind_quoted: [opts: opts] do
+      require GenAsyncCall
+
+      @behaviour GenAsyncCall.GenStateMachine
+
+      def handle_async_reply({:ok, reply}, _tag, _state, _data) do
+        # We do this to trick Dialyzer to not complain about non-local returns. Hat tip GenServer.
+        case :erlang.phash2(1, 1) do
+          0 ->
+            raise "a reply that should have been handled by an await or inline callback was not, or you forget to implement handle_async_reply/4"
+
+          1 ->
+            {:stop, {:unhandled_reply, reply}}
+        end
+      end
+
+      def handle_async_reply({:error, error}, tag, state, data) do
+        # We do this to trick Dialyzer to not complain about non-local returns. Hat tip GenServer.
+        case :erlang.phash2(1, 1) do
+          0 ->
+            case error do
+              {:nodedown, node} ->
+                exit({{:nodedown, node}, {__MODULE__, :handle_async_reply, [error, tag, state, data]}})
+              {:down, reason} ->
+                exit({reason, {__MODULE__, :handle_async_reply, [error, tag, state, data]}})
+              :timeout ->
+                exit({:timeout, {__MODULE__, :handle_async_reply, [error, tag, state, data]}})
+            end
+
+          1 ->
+            {:stop, {:call_errored, error}}
+        end
+      end
+
+      def interposer(reply, {_mref, _tref, fref_or_tag}, state, data) when is_function(fref_or_tag) do
+        fref_or_tag.(reply, state, data)
+      end
+
+      def interposer(reply, {_mref, _tref, fref_or_tag}, state, data) when GenAsyncCall.is_mod_fun_arg(fref_or_tag) do
+        {mod, fname, args} = fref_or_tag
+        apply(mod, fname, [reply, state, data | args])
+      end
+
+      def interposer(reply, {mref, _tref, fref_or_tag}, state, data) do
+        case fref_or_tag do
+          nil ->
+            handle_async_reply(reply, mref, state, data)
+          tag ->
+            handle_async_reply(reply, tag, state, data)
+        end
+      end
+
+      def push_refs(state, data, refs) do
+        GenAsyncCall.push_refs(refs)
+
+        {state, data}
+      end
+
+      def pop_refs(state, data, mref) do
+        refs = GenAsyncCall.pop_refs(mref)
+
+        {refs, state, data}
+      end
+
+      defoverridable handle_async_reply: 4, interposer: 4, push_refs: 3, pop_refs: 3
+
+      def handle_event(:info, {mref, reply}, state, data) when is_reference(mref) do
+        case pop_refs(state, data, mref) do
+          {nil, _state, _data} ->
+            # No match, drop message
+            :keep_state_and_data
+          {refs, state, data} ->
+            GenAsyncCall.cancel_timer(refs)
+            Process.demonitor(mref, [:flush])
+
+            interposer({:ok, reply}, refs, state, data)
+        end
+      end
+
+      def handle_event(:info, {:DOWN, mref, _, process, :noconnection}, state, data) do
+        case pop_refs(state, data, mref) do
+          {nil, _state, _data} ->
+            # No match, drop message
+            :keep_state_and_data
+          {refs, state, data} ->
+            GenAsyncCall.cancel_timer(refs)
+            node = GenAsyncCall.get_node(process)
+
+            interposer({:error, {:nodedown, node}}, refs, state, data)
+        end
+      end
+
+      def handle_event(:info, {:DOWN, mref, _, _, reason}, state, data) do
+        case pop_refs(state, data, mref) do
+          {nil, _state, _data} ->
+            # No match, drop message
+            :keep_state_and_data
+          {refs, state, data} ->
+            GenAsyncCall.cancel_timer(refs)
+
+            interposer({:error, {:down, reason}}, refs, state, data)
+        end
+      end
+
+      def handle_event(:info, {:async_call_timeout, mref}, state, data) do
+        case pop_refs(state, data, mref) do
+          {nil, _state, _data} ->
+            # No match, drop message
+            :keep_state_and_data
+          {refs, state, data} ->
+            Process.demonitor(mref, [:flush])
+
+            interposer({:error, :timeout}, refs, state, data)
+        end
+      end
+
+      defoverridable handle_event: 4
+    end
+  end
+end
---
+++ test/processes/gen_state_machine_client.exs
@@ -0,0 +1,74 @@
+defmodule GenStateMachineClient do
+  use GenStateMachine
+  use GenAsyncCall.GenStateMachine
+  require Logger
+
+  def test_async_sleep(pid, time, opts \\ []) do
+    GenStateMachine.call(pid, {:test_async_sleep, time, opts})
+  end
+
+  def test_async_sleep_fn(pid, time, opts \\ []) do
+    GenStateMachine.call(pid, {:test_async_sleep_fn, time, opts})
+  end
+
+  def test_async_sleep_mfa(pid, time, opts \\ []) do
+    GenStateMachine.call(pid, {:test_async_sleep_mfa, time, opts})
+  end
+
+  def handle_event({:call, from}, {:test_async_sleep, time, opts}, state, data) do
+    refs = TestServer.async_sleep(data.server, time, opts)
+
+    {_state, data} = push_refs(state, data, refs)
+    data = put_in(data, [:from], from)
+
+    {:keep_state, data}
+  end
+
+  def handle_event({:call, from}, {:test_async_sleep_fn, time, opts}, state, data) do
+    refs = TestServer.async_sleep(data.server, time, opts, fn(reply, _state, _data) ->
+      GenStateMachine.reply(from, {:fn, reply})
+
+      :keep_state_and_data
+    end)
+
+    {_state, data} = push_refs(state, data, refs)
+
+    {:keep_state, data}
+  end
+
+  def handle_event({:call, from}, {:test_async_sleep_mfa, time, opts}, state, data) do
+    refs = TestServer.async_sleep(data.server, time, opts, {__MODULE__, :my_mfa, [from]})
+
+    {_state, data} = push_refs(state, data, refs)
+
+    {:keep_state, data}
+  end
+
+  def handle_event(event_type, event_content, state, data) do
+    super(event_type, event_content, state, data)
+  end
+
+  def my_mfa(reply, _state, _data, from) do
+    GenStateMachine.reply(from, {:mfa, reply})
+
+    :keep_state_and_data
+  end
+
+  def handle_async_reply({:ok, reply}, _tag, _state, data) do
+    GenStateMachine.reply(data.from, reply)
+
+    {:keep_state, %{data | from: nil}}
+  end
+
+  def handle_async_reply(reply, tag, state, data) do
+    super(reply, tag, state, data)
+  end
+
+  def start_link(init_args) do
+    GenStateMachine.start_link(__MODULE__, init_args)
+  end
+
+  def init(server) do
+    {:ok, :ready, %{server: server, from: nil}}
+  end
+end
